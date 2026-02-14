409e235f54a771e96867fb8571f987216c5529f1
GH050800 <grno@outlook.com>
2026-02-14 18:12:32 +0400

    fix

--- 101_multithread/main.cpp
+++ 101_multithread/main.cpp
@@ -6,17 +6,23 @@
 
 constexpr size_t size = 1000 * 1000 * 1000;
 
-void fill_and_calc(std::vector<size_t>& vec, size_t start, size_t end, size_t& sum)
+void fill(std::vector<size_t>& vec, size_t start, size_t end)
+{
+    for (size_t i = start; i < end; ++i)
+    {
+        vec[i] = (start + 58 + i * 21313 - 4564) % 100;
+    }
+}
+
+void calc(std::vector<size_t>& vec, size_t start, size_t end, size_t& sum)
 {
     size_t temp = 0;
     for (size_t i = start; i < end; ++i)
     {
-        vec[i] = (start + 58 + i * 21313 - 4564) % 100;
         temp += vec[i];
     }
     sum = temp;
 }
-
 
 int main()
 {
@@ -28,11 +34,19 @@ int main()
     size_t step = size / 5;
     size_t sums[5] = { 0 };
 
-	std::thread t1(fill_and_calc, std::ref(vec), 0, step, std::ref(sums[0]));
-    std::thread t2(fill_and_calc, std::ref(vec), step, step * 2, std::ref(sums[1]));
-    std::thread t3(fill_and_calc, std::ref(vec), step * 2, step * 3, std::ref(sums[2]));
-    std::thread t4(fill_and_calc, std::ref(vec), step * 3, step * 4, std::ref(sums[3]));
-    std::thread t5(fill_and_calc, std::ref(vec), step * 4, size, std::ref(sums[4]));
+	std::thread t1(fill, std::ref(vec), 0, step);
+    std::thread t2(fill, std::ref(vec), step, step * 2);
+    std::thread t3(fill, std::ref(vec), step * 2, step * 3);
+    std::thread t4(fill, std::ref(vec), step * 3, step * 4);
+    std::thread t5(fill, std::ref(vec), step * 4, size);
+
+    t1.join(); t2.join(); t3.join(); t4.join(); t5.join();
+
+    t1 = std::thread(calc, std::ref(vec), 0, step, std::ref(sums[0]));
+    t2 = std::thread(calc, std::ref(vec), step, step * 2, std::ref(sums[1]));
+    t3 = std::thread(calc, std::ref(vec), step * 2, step * 3, std::ref(sums[2]));
+    t4 = std::thread(calc, std::ref(vec), step * 3, step * 4, std::ref(sums[3]));
+    t5 = std::thread(calc, std::ref(vec), step * 4, size, std::ref(sums[4]));
 
     t1.join(); t2.join(); t3.join(); t4.join(); t5.join();
 
