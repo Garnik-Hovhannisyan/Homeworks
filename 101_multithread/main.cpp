c1f8b757dff7ca5283bc109068b37ac610a89fe0
GH050800 <grno@outlook.com>
2026-02-16 14:22:08 +0400

    .

--- 101_multithread/main.cpp
+++ 101_multithread/main.cpp
@@ -2,63 +2,77 @@
 #include <chrono>
 #include <thread>
 #include <vector>
-
+#include <ctime>
+#include <future>
 
 constexpr size_t size = 1000 * 1000 * 1000;
+const int thread_count = std::thread::hardware_concurrency();
 
-void fill(std::vector<size_t>& vec, size_t start, size_t end)
+void fill(std::vector<char>& vec)
 {
-    for (size_t i = start; i < end; ++i)
+    std::vector<std::thread> vec_threads;
+    size_t step = size / thread_count;
+    
+    for (int i = 0; i < thread_count; i++)
     {
-        vec[i] = (start + 58 + i * 21313 - 4564) % 100;
+        size_t start = i * step;
+        size_t end = (i + 1) * step;
+
+        vec_threads.emplace_back([&, start, end]()
+        {
+            for (size_t j = start; j < end; j++) vec[j] = rand() % 100;
+		});
     }
+
+    for (auto& thread : vec_threads) thread.join();
 }
 
-void calc(std::vector<size_t>& vec, size_t start, size_t end, size_t& sum)
+size_t calc(std::vector<char>& vec)
 {
-    size_t temp = 0;
-    for (size_t i = start; i < end; ++i)
+    std::vector<std::thread> vec_threads;
+    std::vector<size_t> sums(thread_count, 0);
+
+    int num_threads = std::thread::hardware_concurrency();
+    size_t step = size / thread_count;
+
+    for (int i = 0; i < thread_count; i++)
     {
-        temp += vec[i];
+        size_t start = i * step;
+        size_t end = (i + 1) * step;
+
+        vec_threads.emplace_back([&, start, end, i]()
+            {
+                size_t temp = 0;
+                for (size_t j = start; j < end; j++) temp += vec[j];
+                sums[i] = temp;
+            });
     }
-    sum = temp;
+
+    for (auto& thread : vec_threads) thread.join();
+
+    size_t finalSum = 0;
+    for (auto& s : sums) finalSum += s;
+    return finalSum;
 }
 
 int main()
 {
-    auto start_time = std::chrono::high_resolution_clock::now();
-
-    std::vector<size_t> vec;
+    std::vector<char> vec;
     vec.resize(size);
 
-    size_t step = size / 5;
-    size_t sums[5] = { 0 };
+    auto fill_start = std::chrono::high_resolution_clock::now();
+    fill(vec);
+    auto fill_end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> fill_duration = fill_end - fill_start;
 
-	std::thread t1(fill, std::ref(vec), 0, step);
-    std::thread t2(fill, std::ref(vec), step, step * 2);
-    std::thread t3(fill, std::ref(vec), step * 2, step * 3);
-    std::thread t4(fill, std::ref(vec), step * 3, step * 4);
-    std::thread t5(fill, std::ref(vec), step * 4, size);
+    auto calc_start = std::chrono::high_resolution_clock::now();
+    size_t result = calc(vec);
+    auto calc_end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> calc_duration = calc_end - calc_start;
 
-    t1.join(); t2.join(); t3.join(); t4.join(); t5.join();
-
-    t1 = std::thread(calc, std::ref(vec), 0, step, std::ref(sums[0]));
-    t2 = std::thread(calc, std::ref(vec), step, step * 2, std::ref(sums[1]));
-    t3 = std::thread(calc, std::ref(vec), step * 2, step * 3, std::ref(sums[2]));
-    t4 = std::thread(calc, std::ref(vec), step * 3, step * 4, std::ref(sums[3]));
-    t5 = std::thread(calc, std::ref(vec), step * 4, size, std::ref(sums[4]));
-
-    t1.join(); t2.join(); t3.join(); t4.join(); t5.join();
-
-    size_t finalSum = 0;
-    for (size_t i = 0; i < 5; ++i) finalSum += sums[i];
-    
-    auto end_time = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> duration = end_time - start_time;
-
-	std::cout << "Sum: " << finalSum << std::endl;
-    std::cout << "Time: " << duration.count() << ".s" << std::endl;
-    
+    std::cout << "Fill time: " << fill_duration.count() << ".s" << std::endl;
+    std::cout << "Calc time: " << calc_duration.count() << ".s" << std::endl;
+	std::cout << "Sum: " << result << std::endl;
     return 0;
 }
 
